d47d12d9ae4d31f5d94648175436d9214efb08a8
cwchiu <sisimi.pchome@gmail.com>
2013-04-05 11:41:44 +0800

    remove comment

--- code_coverage_prepend.php
+++ code_coverage_prepend.php
@@ -1,9 +1,6 @@
 <?php
 // REF: http://shiftlock.wordpress.com/2009/11/01/standalone-php-code-coverage-without-phpunit/
 function __code_coverage_xdebug_stop(){
-    //$data = xdebug_get_code_coverage();
-    //xdebug_stop_code_coverage();
-    
     include_once 'code_coverage_class.php';
     $code_coverage = new code_coverage('code_coverage.var','code_coverage_reports/');
     $code_coverage->code_coverage_analysis_save();
